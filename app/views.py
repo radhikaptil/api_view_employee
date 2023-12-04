4c26ac705663f6a29cb37a6e1809715eccdf30df
Radhika <139960980+radhikaptil@users.noreply.github.com>
2023-12-04 17:53:37 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from app.models import *
 from app.serializers import *
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
 def EmployeeData(request):
 
     EMPDATA=Employee.objects.all()
